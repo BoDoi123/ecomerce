b5ca63528abceeb13f1eb854227f38396cdbf321
Chu Long <chulong_t65@hus.edu.vn>
2025-08-16 13:02:59 +0700

    Thêm chức năng refresh token khi token hết hạn (Chưa hoàn thiện)

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,58 @@ import { Fragment, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import DefaultComponent from "./components/DefaultComponent/DefaultComponent";
 import { routes } from "./routes";
-// import { useQuery } from "@tanstack/react-query";
+import { isJsonString } from "./utils";
+import { jwtDecode } from "jwt-decode";
+import * as UserService from "./services/UserService";
+import { useDispatch } from "react-redux";
+import { updateUser } from "./redux/slides/userSlide";
 
 function App() {
-	// useEffect(() => {
-	// 	fetchApi();
-	// }, []);
+	const dispatch = useDispatch();
 
-	// const fetchApi = async () => {
-	// 	const res = await axios.get(
-	// 		`${import.meta.env.VITE_API_URL_BACKEND}/product/get-all`
-	// 	);
+	useEffect(() => {
+		const { storageData, decoded } = handleDecoded();
 
-	// 	return res.data;
-	// };
+		if (decoded?.id) {
+			handleGetDetailsUser(decoded?.id, storageData);
+		}
+	}, []);
 
-	// const query = useQuery({ queryKey: ["todos"], queryFn: fetchApi });
-	// console.log("query", query);
+	const handleDecoded = () => {
+		let storageData = localStorage.getItem("access_token");
+		let decoded = {};
+
+		if (storageData && isJsonString(storageData)) {
+			storageData = JSON.parse(storageData);
+
+			decoded = jwtDecode(storageData);
+		}
+
+		return { decoded, storageData };
+	};
+
+	UserService.axiosJWT.interceptors.request.use(
+		async (config) => {
+			const currentTime = new Date();
+			const { decoded } = handleDecoded();
+
+			if (decoded?.exp < currentTime.getTime() / 1000) {
+				const data = await UserService.refreshToken();
+				config.headers["token"] = `Bearer ${data?.access_token}`;
+			}
+
+			return config;
+		},
+		(error) => {
+			return Promise.reject(error);
+		}
+	);
+
+	const handleGetDetailsUser = async (id, token) => {
+		const res = await UserService.getDetailsUser(id, token);
+
+		dispatch(updateUser({ ...res?.data, access_token: token }));
+	};
 
 	return (
 		<div>
