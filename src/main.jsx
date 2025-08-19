b54fb5581ce96720f36214ec4dd9892c8296a80b
Chu Long <chulong_t65@hus.edu.vn>
2025-08-20 02:28:45 +0700

    tích hợp redux persis với redux-toolkit

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,10 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
-import { store } from "./redux/store";
+import { persistor, store } from "./redux/store";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { PersistGate } from "redux-persist/integration/react";
 
 const queryClient = new QueryClient();
 
@@ -13,7 +14,9 @@ createRoot(document.getElementById("root")).render(
 	// <StrictMode>
 	<QueryClientProvider client={queryClient}>
 		<Provider store={store}>
-			<App />
+			<PersistGate loading={null} persistor={persistor}>
+				<App />
+			</PersistGate>
 		</Provider>
 		<ReactQueryDevtools initialIsOpen={false} />
 	</QueryClientProvider>
